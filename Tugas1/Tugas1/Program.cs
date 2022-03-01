b307ca93fb6e39f05a057cab1e86e3c4ce5264ab
BrayenTobing <92644814+BrayenTobing@users.noreply.github.com>
2022-03-01 19:31:54 +0700

    Add files via upload

---
+++ Tugas1/Tugas1/Program.cs
@@ -0,0 +1,21 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: acer
+ * Date: 01/03/2022
+ * Time: 19:04
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace Tugas1
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			Console.ReadKey(true);
+		}
+	}
+}
---
+++ Tugas1b/Tugas1b/Program.cs
@@ -0,0 +1,26 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: acer
+ * Date: 01/03/2022
+ * Time: 19:06
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace Tugas1b
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.Write("Nama kamu siapa? ");
+			
+			string nama = Console.ReadLine();
+			Console.WriteLine("Nama kamu"+nama);
+			
+			Console.Write("Press any key to continue . . .");
+			Console.ReadKey(true);
+		}
+	}
+}
